6cc4971b1efc3cd6efe9daf43062744a612f4668
okkdev <dev@stehlik.me>
2022-05-27 13:37:24 +0200

    update 👁️

--- src/main.jsx
+++ src/main.jsx
@@ -3,33 +3,52 @@ const refreshFrequency = 5000 // ms
 const command = `
 BATTERY=$(bash bar/scripts/battery.sh)
 TIME=$(date +"%H:%M")
+DATE=$(date +"%a, %b %d")
 SPOTIFY=$(osascript bar/scripts/spotify.scpt)
 
 echo $(cat <<-EOF
 {
   "battery": "$BATTERY",
   "time": "$TIME",
+  "date": "$DATE",
   "spotify": "$SPOTIFY"
 }
 EOF
 )
 `
 
+const initialState = {
+  output: {
+    battery: "Loading",
+    time: "00:00",
+    date: "Loading",
+    spotify: "",
+  },
+}
+
+const updateState = (event, previousState) => {
+  try {
+    return { output: JSON.parse(event.output) }
+  } catch (err) {
+    return previousState
+  }
+}
+
 const render = ({ output }) => {
-  const data = JSON.parse(output)
   return (
     <div className="w-screen">
       <link href="bar/style.css" type="text/css" rel="stylesheet" />
       <div className="flex justify-between gate">
         <div></div>
         <div className="flex gap-2">
-          {data.spotify ? <div className="blob">♫ {data.spotify}</div> : ""}
-          <div className="blob">→ {data.time}</div>
-          <div className="blob">{data.battery}</div>
+          {output.spotify ? <div className="blob">♫ {output.spotify}</div> : ""}
+          <div className="blob">→ {output.date}</div>
+          <div className="blob">☀ {output.time}</div>
+          <div className="blob">{output.battery}</div>
         </div>
       </div>
     </div>
   )
 }
 
-export { refreshFrequency, command, render }
+export { refreshFrequency, command, initialState, updateState, render }
